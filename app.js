b2e23f8ac747363ba44b2a339dee6cd5e9f45375
Ashishkabirpanthi <ashishkabirpanthi@gmail.com>
2025-07-20 18:50:42 +0530

    done

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const io = socketIO(server);
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/", indexRouter);
@@ -19,27 +19,44 @@ let waitingusers = [];
 let roomnames = {
 };
 
-io.on('connection', function(socket){
-    socket.on('joinroom', function(){
-        if(waitingusers.length > 0){
+io.on('connection', function (socket) {
+    socket.on('joinroom', function () {
+        if (waitingusers.length > 0) {
             let partner = waitingusers.shift();
             const roomname = `${socket.id}-${partner.id}`;
             socket.join(roomname);
             partner.join(roomname);
-            io.to(roomname).emit("joined" ,roomname);
+            io.to(roomname).emit("joined", roomname);
         }
-        else{
+        else {
             waitingusers.push(socket);
             console.log("you are in waiting-list");
         };
     });
 
-    socket.on('message', function(data){
+    socket.on('signalingMessage', function (data) {
+        socket.broadcast.to(data.room).emit("signalingMessage", data.message);
+    });
+
+    socket.on('message', function (data) {
         socket.broadcast.to(data.room).emit("message", data.message);
     })
 
-    socket.on('disconnect', function(socket){
+    socket.on("startVideoCall", function ({ room }) {
+        socket.broadcast.to(room).emit("incomingCall");
+    });
+
+    socket.on("rejectCall", function ({ room }) {
+        socket.broadcast.to(room).emit("callRejected");
+    });
+
+    socket.on("acceptCall", function ({ room }) {
+        socket.broadcast.to(room).emit("callAccepted");
+    });
+
+    socket.on('disconnect', function (socket) {
         let index = waitingusers.findIndex(index => index === socket.id);
     });
+
 })
 server.listen(3000, () => console.log('Server is running on port 3000'));
